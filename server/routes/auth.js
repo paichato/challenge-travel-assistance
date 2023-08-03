9da04cdf2f5a9c2088052847fccf75075ae259da
paichato <tsenane14@gmail.com>
2023-08-03 10:35:27 +0300

    feat: add tweaks to auth
    feat: added tweaks to auth and dash

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -12,26 +12,19 @@ const registerSchema = Joi.object({
 });
 
 router.post("/register", async (req, res) => {
-  //CHECKING IF USER EMAIL ALREADY EXISTS
-
   const { username, email, password } = req.body;
 
   if (!username || !password)
     return res.status(401).send("Missing username or password");
 
   const userExist = await User.findOne({ username });
-  // IF EMAIL EXIST THEN RETURN
   if (userExist) {
     res.status(400).send("User already exists");
     return;
   }
 
-  //HASHING THE PASSWORD
-
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
-
-  //ON PROCESS OF ADDING NEW USER
 
   const newUser = new User({
     username,
@@ -40,18 +33,11 @@ router.post("/register", async (req, res) => {
   });
 
   try {
-    //VALIDATION OF USER INPUTS
-
     const { error } = await registerSchema.validateAsync(req.body);
-    //WE CAN JUST GET THE ERROR(IF EXISTS) WITH OBJECT DECONSTRUCTION
-
-    //   IF ERROR EXISTS THEN SEND BACK THE ERROR
     if (error) {
       res.status(400).send(error.details[0].message);
       return;
     } else {
-      //NEW USER IS ADDED
-
       const savedUser = await newUser.save();
       const token = jwt.sign({ _id: savedUser._id }, process.env.TOKEN_SECRET, {
         subject: username,
@@ -76,10 +62,7 @@ const loginSchema = Joi.object({
   password: Joi.string().min(6).required(),
 });
 
-//LOGIN USER
-
 router.post("/login", async (req, res) => {
-  //CHECKING IF USER EMAIL EXISTS
   const { username, password } = req.body;
 
   if (!username || !password)
@@ -88,19 +71,14 @@ router.post("/login", async (req, res) => {
   const user = await User.findOne({ username });
   if (!user) return res.status(400).send("Username or Password is incorrect");
 
-  //CHECKING IF USER PASSWORD MATCHES
-
   const validPassword = await bcrypt.compare(req.body.password, user.password);
   if (!validPassword)
     return res.status(400).send("Username or password is incorrect");
 
   try {
-    //VALIDATION OF USER INPUTS
-
     const { error } = await loginSchema.validateAsync(req.body);
     if (error) return res.status(400).send(error.details[0].message);
     else {
-      //SENDING BACK THE TOKEN
       const token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET, {
         subject: username,
         expiresIn: "1d",
