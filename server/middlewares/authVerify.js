461a225470a0264667e66a69cf4153d251ba7f27
paichato <tsenane14@gmail.com>
2023-08-04 00:39:23 +0300

    feat: add tweaks to server

--- server/middlewares/authVerify.js
+++ server/middlewares/authVerify.js
@@ -3,7 +3,11 @@ const { TOKEN_NAME } = require("../utils/constants");
 
 module.exports = function async(req, res, next) {
   const token = req.header(TOKEN_NAME);
-  if (!token) return res.status(401).send("Access Denied");
+  // if (!token) return res.status(401).send("Access Denied");
+  if (!token) {
+    req.user = {};
+    return next();
+  }
 
   try {
     const verified = jwt.verify(token.split(" ")[1], process.env.TOKEN_SECRET);
--- server/routes/authDashboard.js
+++ server/routes/authDashboard.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
 const verify = require("../middlewares/authVerify");
-
+const axios = require("axios");
 const router = require("express").Router();
 
 router.get("/allusers", verify, async (req, res) => {
@@ -10,6 +10,57 @@ router.get("/allusers", verify, async (req, res) => {
   } catch (error) {
     res.status(500).send(error);
   }
+});
+
+router.get("/travel/:city", verify, async (req, res) => {
+  // res.send(req.user);
+  const city = req.params.city;
+  console.log("city:", city);
+
+  const fetchedData = [{ user: req.user }];
+
+  axios
+    .get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.OPEN_WEATHER_SECRET}&units=metric`
+    )
+    .then(function (weather) {
+      console.log(weather.data);
+      fetchedData.push({ weather: weather.data });
+
+      const { country } = weather.data.sys;
+
+      if (!country) {
+        return res.status(400).send("No country found");
+      }
+
+      axios
+        .get(
+          `https://api.worldbank.org/v2/country/${country}/indicator/NY.GDP.MKTP.CD;NY.GDP.PCAP.CD;SP.POP.TOTL?source=2&mrv=1&format=json`
+        )
+        .then(function (countryData) {
+          console.log(countryData.data[1]);
+          fetchedData.push({ country: countryData.data[1] });
+          return res.status(200).json(fetchedData);
+        })
+        .catch(function (error) {
+          JSON.stringify(error);
+          const errorLog = {
+            message: "Error fetching country data",
+            error: error?.response?.data,
+            fullError: error,
+          };
+          return res.status(400).json(errorLog);
+        });
+    })
+    .catch(function (error) {
+      console.log(JSON.stringify(error));
+      const errorLog = {
+        message: "Error fetching weather data",
+        error: error?.response?.data,
+        fullError: error,
+      };
+      return res.status(400).json(errorLog);
+    });
 });
 
 module.exports = router;
